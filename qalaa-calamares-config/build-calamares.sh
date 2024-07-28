e202b0a716323c843dc4bfc8155237be104076f5
MILOUDIAS <adelhani.miloudi@gmail.com>
2024-07-28 16:00:39 +0100

    Updated calamares config

--- qalaa-calamares-config/build-calamares.sh
+++ qalaa-calamares-config/build-calamares.sh
@@ -1,39 +1,63 @@
 #!/bin/bash
+#CHROOT=$HOME/Documents/chroot-archlinux
+#https://wiki.archlinux.org/index.php/DeveloperWiki:Building_in_a_Clean_Chroot
+#https://archlinux.org/news/git-migration-completed/
+#https://wiki.archlinux.org/title/DeveloperWiki:HOWTO_Be_A_Packager
 
 destination1="/mnt/fda14e36-49c8-4508-a4b0-f37189565cd9/iso/QalaaLinux/qalaa_repo1/x86_64/"
 
 destiny=$destination1
 
-search1="qalaa-calamares"
-search=$search1
+# 2. makepkg"
+# 1. chroot"
 
-tput setaf 2
+pwdpath=$(echo $PWD)
+pwd=$(basename "$PWD")
+
+#which packages are always going to be build with makepkg or choice 2
+#makepkglist=""
+
+search1=$(basename "$PWD")
+search2=qalaalinux
+
+search=$search1
+rm -rf /tmp/tempbuild
+if test -f "/tmp/tempbuild"; then
+	rm /tmp/tempbuild
+fi
+mkdir /tmp/tempbuild
+cp -r $pwdpath/* /tmp/tempbuild/
+#cp -r $pwdpath/.* /tmp/tempbuild
+
+cd /tmp/tempbuild/
+
+tput setaf 3
 echo "#############################################################################################"
-echo "#########        Let us build the package "$(basename $(pwd))
+echo "#########        Let us build the package with MAKEPKG "$(basename $(pwd))
 echo "#############################################################################################"
 tput sgr0
-
-updpkgsums
-
 makepkg -s
 
 echo "Moving created files to " $destiny
 echo "#############################################################################################"
-mv $search*pkg.tar.zst $destiny
-
+mv -n $search*pkg.tar.zst $destiny
+# mv -n $search*pkg.tar.zst.sig $destiny
 echo "Cleaning up"
 echo "#############################################################################################"
 echo "deleting unnecessary folders"
 echo "#############################################################################################"
 
-rm -rf pkg src qalaa-calamares-config*
+if [[ -f $wpdpath/*.log ]]; then
+	rm $pwdpath/*.log
+fi
+if [[ -f $wpdpath/*.deb ]]; then
+	rm $pwdpath/*.deb
+fi
+if [[ -f $wpdpath/*.tar.gz ]]; then
+	rm $pwdpath/*.tar.gz
+fi
 
-echo "deleting unnecessary files"
-echo "#############################################################################################"
-
-rm -rf calamares
-
-tput setaf 8
+tput setaf 11
 echo "#############################################################################################"
 echo "###################                       build done                   ######################"
 echo "#############################################################################################"
